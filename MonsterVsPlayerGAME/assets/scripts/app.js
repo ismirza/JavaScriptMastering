a97fd8713c9acf72d514d9655849aeda27ae3f7f
Ib <ismirza@pointpark.edu>
2021-01-05 18:12:58 -0500

    Update app.js
    Logging Attacks and Heals

--- MonsterVsPlayerGAME/assets/scripts/app.js
+++ MonsterVsPlayerGAME/assets/scripts/app.js
@@ -3,13 +3,17 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-
 const MODE_ATTACK = 'ATTACK';
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK';
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
 
 const enteredValue = prompt('Max life for player and monster.', '100');
 
 let chosenMaxLife = parseInt(enteredValue);
+let battleLog = [];
  
 if (isNaN(chosenMaxLife) || chosenMaxLife <=0 ) {
     chosenMaxLife = 100;
@@ -18,6 +22,28 @@ if (isNaN(chosenMaxLife) || chosenMaxLife <=0 ) {
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
+
+function writeToLog(event, value, monsterHealth, playerHealth) {
+    let logEntry;
+    if (event === LOG_EVENT_PLAYER_ATTACK) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'MONSTER',
+            finalMonsterHealth: currentMonsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+        battleLog.push(logEntry);
+    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'PLAYER',
+            finalMonsterHealth: currentMonsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    }   else if ()
+}
 
 adjustHealthBars(chosenMaxLife);
 
